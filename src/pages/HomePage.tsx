9ea5a2302f3c4cf5fd0d6852af746ae1a449a1fb
Alexander Van Maele <alexander@greenisland.be>
2023-05-12 22:44:33 +0200

    ✨ Close #5 Add audio player

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -1,10 +1,16 @@
-import styles from './HomePage.module.css';
+import styles from './HomePage.module.scss';
 
 const HomePage = () => (
   <div className={styles.root}>
-    <audio controls src="/src/assets/audio/guru_yoga_lama_achuk_tibetan_web.mp3">
-      <a href="/src/assets/audio/guru_yoga_lama_achuk_tibetan_web.mp3">Download audio</a>
-    </audio>
+    <div className={styles.pageContainer}>
+      <audio
+        className={styles.audioPlayer}
+        controls
+        src="/src/assets/audio/guru_yoga_lama_achuk_tibetan_web.mp3"
+      >
+        <a href="/src/assets/audio/guru_yoga_lama_achuk_tibetan_web.mp3">Download audio</a>
+      </audio>
+    </div>
   </div>
 );
 
