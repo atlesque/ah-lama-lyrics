f2547191f81e47d5ae4f1405e25ba574d00deeaa
Alexander Van Maele <alexander@greenisland.be>
2023-05-12 22:30:03 +0200

    🔧 Attempt to add scss typing

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -1,13 +1,11 @@
-import styles from './HomePage.module.scss';
+import styles from './HomePage.module.css';
 
-const HomePage = () => {
-  return (
-    <div className={styles.root}>
-      <audio controls src="/src/assets/audio/guru_yoga_lama_achuk_tibetan_web.mp3">
-        <a href="/src/assets/audio/guru_yoga_lama_achuk_tibetan_web.mp3">Download audio</a>
-      </audio>
-    </div>
-  );
-};
+const HomePage = () => (
+  <div className={styles.root}>
+    <audio controls src="/src/assets/audio/guru_yoga_lama_achuk_tibetan_web.mp3">
+      <a href="/src/assets/audio/guru_yoga_lama_achuk_tibetan_web.mp3">Download audio</a>
+    </audio>
+  </div>
+);
 
 export default HomePage;
